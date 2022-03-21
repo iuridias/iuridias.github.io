01297d7315f8ebe8fac02f27fc0a69439c520f48
Iuri Dias <iuridias.mw@gmail.com>
2022-03-21 17:55:28 -0300

    Adicionada versão mobile First

--- js/index.js
+++ js/index.js
@@ -4,3 +4,29 @@ const menu = document.querySelector('.menu-lateral');
  botaoMenu.addEventListener('click', () => {
    menu.classList.toggle('menu-lateral--ativo');
  })
+
+const menuHome = document.querySelector('.menu-lateral__link-inicio');
+const menuSobre = document.querySelector('.menu-lateral__link-sobre');
+const menuHabilidades = document.querySelector('.menu-lateral__link-habilidades');
+const menuProjetos = document.querySelector('.menu-lateral__link-projetos');
+const menuContato = document.querySelector('.menu-lateral__link-contato');
+
+menuHome.addEventListener('click', () => {
+  menu.classList.toggle('menu-lateral--ativo');
+})
+
+menuSobre.addEventListener('click', () => {
+  menu.classList.toggle('menu-lateral--ativo');
+})
+
+menuHabilidades.addEventListener('click', () => {
+  menu.classList.toggle('menu-lateral--ativo');
+})
+
+menuProjetos.addEventListener('click', () => {
+  menu.classList.toggle('menu-lateral--ativo');
+})
+
+menuContato.addEventListener('click', () => {
+  menu.classList.toggle('menu-lateral--ativo');
+})
